15029ad27ab7c8adf3eecbeb5ee938ff22dbf5da
Tyler C. Yates <tyates907@gmail.com>
2020-08-10 23:14:19 -0700

    rewrite bot for discord.js

--- bot.js
+++ bot.js
@@ -1,15 +1,16 @@
 const Discord = require('discord.js');
 const DiscordOpus = require('@discordjs/opus');
-const auth = './auth'
+const config = require('./config.json')
 
 // Initialize Discord Bot
 const bot = new Discord.Client();
-
+bot.login(config.botToken);
 bot.once('ready', () => {
-    console.log('You did it!');
+    console.log("Ready for Disco \n♪♪\\('O')/♪♪");
 }); 
 
-async () => {
-    console.log('calling')
-    const login = await bot.login(auth.botToken);
-};
+bot.on('message', message => {
+    if (message.content === '!ping') {
+        message.channel.send('Pong');
+    }
+});
