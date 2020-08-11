dd61cae505bd49457e3a425abe3376c155807f08
Tyler C. Yates <tyates907@gmail.com>
2020-08-10 22:39:50 -0700

    change dependencies (discord.js)

--- bot.js
+++ bot.js
@@ -1,39 +1,15 @@
-var Discord = require('discord.io');
-var logger = require('winston');
-var auth = require('./auth.json');
-// Configure logger settings
-logger.remove(logger.transports.Console);
-logger.add(new logger.transports.Console, {
-    colorize: true
-});
-logger.level = 'debug';
-// Initialize Discord Bot
-var bot = new Discord.Client({
-    token: auth.token,
-    autorun: true
-});
-bot.on('ready', function (evt) {
-    logger.info('Connected');
-    logger.info('Logged in as: ');
-    logger.info(bot.username + ' - (' + bot.id + ')');
-});
-bot.on('message', function (user, userID, channelID, message, evt) {
-    // Our bot needs to know if it will execute a command
-    // It will listen for messages that will start with `!`
-    if (message.substring(0, 1) == '!') {
-        var args = message.substring(1).split(' ');
-        var cmd = args[0];
+const Discord = require('discord.js');
+const DiscordOpus = require('@discordjs/opus');
+const auth = './auth'
 
-        args = args.splice(1);
-        switch (cmd) {
-            // !ping
-            case 'ping':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'Pong!'
-                });
-                break;
-            // Just add any case commands if you want to..
-        }
-    }
-});
+// Initialize Discord Bot
+const bot = new Discord.Client();
+
+bot.once('ready', () => {
+    console.log('You did it!');
+}); 
+
+async () => {
+    console.log('calling')
+    const login = await bot.login(auth.botToken);
+};
