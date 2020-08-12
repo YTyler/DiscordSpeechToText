929f918b837979eb0a240d50307907ee3394c4a8
Tyler C. Yates <tyates907@gmail.com>
2020-08-11 17:02:49 -0700

    edit !ping & add !boop, !server commands

--- bot.js
+++ bot.js
@@ -9,8 +9,21 @@ bot.once('ready', () => {
     console.log("Ready for Disco \n♪♪ \\('O')/ ♪♪");
 }); 
 
+//Bot Commands
 bot.on('message', message => {
-    if (message.content === `${prefix}ping`) {
-        message.channel.send('Pong');
+    if (message.content.startsWith(`${prefix}ping`)) {
+        message.channel.send('pong');
+    } else if (message.content.startsWith(`${prefix}beep`)) {
+        message.channel.send('boop');
+    }
+    else if (message.content.startsWith(`${prefix}server`)) {
+        message.channel.send(
+            `Server Details
+            Name: ${message.guild.name}
+            Description: ${message.guild.description ? message.guild.description : 'No Description' }
+            Total Members: ${message.guild.memberCount}
+            Date Created: ${message.guild.createdAt}
+            Region: ${message.guild.region}`
+        );
     }
 });
